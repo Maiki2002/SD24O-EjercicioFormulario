622efd1ca552f40822abea60a07dcfd1ec21bb7f
Maiki2002 <maiki200202@gmail.com>
2024-11-19 16:59:06 -0600

    Primer commit

--- api.py
+++ api.py
@@ -2,38 +2,38 @@ from fastapi import FastAPI, UploadFile, File, Form
 from typing import Optional
 from pydantic import BaseModel
 import shutil
+import os # Para acceder a la ruta del home 
+import uuid
 
 # creación del servidor
 app = FastAPI()
 
-#definición de la base del usuario
-class UsuarioBase(BaseModel):
-    nombre:Optional[str]=None
-    edad:int
-    domicilio:str    
-    
-usuarios = [{
-    "id": 0,
-    "nombre": "Homero Simpson",
-    "edad": 40,
-    "domicilio": "Av. Simpre Viva"
-}, {
-    "id": 1,
-    "nombre": "Marge Simpson",
-    "edad": 38,
-    "domicilio": "Av. Simpre Viva"
-}, {
-    "id": 2,
-    "nombre": "Lisa Simpson",
-    "edad": 8,
-    "domicilio": "Av. Simpre Viva"
-}, {
-    "id": 3,
-    "nombre": "Bart Simpson",
-    "edad": 10,
-    "domicilio": "Av. Simpre Viva"
-}]
+@app.post("/foto-usuario")
+async def guarda_foto(nombre:str=Form(...),direccion:str=Form(...),fotografia:UploadFile=File(...),vip:Optional[str]=Form(None)):
+    print("Nombre:", nombre)
+    print("Direccion:", direccion)
+    print("Vip:", vip)
+    vip = vip == "true"
+    home_usuario = os.path.expanduser("~") #home usuario
+    nombre_archivo = uuid.uuid4() #nombre del formato hexadecimal
+    extension_foto = os.path.splitext(fotografia.filename)[1]
+    if vip:
+        ruta_imagen = f'{home_usuario}/fotos-usuarios-vip/{nombre_archivo}{extension_foto}'
+    else:
+        ruta_imagen = f'{home_usuario}/fotos-usuarios/{nombre_archivo}{extension_foto}'
 
+    print("Guardado la foto en ", ruta_imagen)
+    with open(ruta_imagen,"wb") as imagen:
+        contenido = await fotografia.read()
+        imagen.write(contenido)
+
+    respuesta = {
+        "Nombre" : nombre,
+        "Direccion": direccion,
+        "Ruta": ruta_imagen,
+        "Vip": vip
+    }
+    return respuesta
 
 # decorator
 @app.get("/")
@@ -44,68 +44,3 @@ def hola_mundo():
     }
 
     return respuesta
-
-
-@app.get("/usuarios/{id}")
-def usuario_por_id(id: int):
-    print("buscando usuario por id:", id)
-    # simulamos consulta a la base:
-    return usuarios[id]
-
-
-@app.get("/usuarios/{id}/compras/{id_compra}")
-def compras_usuario_por_id(id: int, id_compra: int):
-    print("buscando compra con id:", id_compra, " del usuario con id:", id)
-    # simulamos la consulta
-    compra = {
-        "id_compra": 787,
-        "producto": "TV",
-        "precio": 14000
-    }
-
-    return compra
-
-@app.get("/usuarios")
-def lista_usuarios(*,lote:int=10,pag:int,orden:Optional[str]=None): #parametros de consulta ?lote=10&pag=1
-    print("lote:",lote, " pag:", pag, " orden:", orden)
-    #simulamos la consulta
-    return usuarios
-
-@app.post("/usuarios")
-def guardar_usuario(usuario:UsuarioBase, parametro1:str):
-    print("usuario a guardar:", usuario, ", parametro1:", parametro1)
-    #simulamos guardado en la base.
-    
-    usr_nuevo = {}
-    usr_nuevo["id"] = len(usuarios)
-    usr_nuevo["nombre"] = usuario.nombre
-    usr_nuevo["edad"] = usuario.edad
-    usr_nuevo["domicilio"] = usuario.domicilio
-
-    usuarios.append(usuario)
-
-    return usr_nuevo
-
-@app.put("/usuario/{id}")
-def actualizar_usuario(id:int, usuario:UsuarioBase):
-    #simulamos consulta
-    usr_act = usuarios[id]
-    #simulamos la actualización
-    usr_act["nombre"] = usuario.nombre
-    usr_act["edad"] = usuario.edad
-    usr_act["domicilio"] = usuario.domicilio    
-
-    return usr_act
-    
-@app.delete("/usuario/{id}")
-def borrar_usuario(id:int):
-    #simulamos una consulta
-    if id>=0 and id< len(usuarios):
-        usuario = usuarios[id]
-    else:
-        usuario = None
-    
-    if usuario is not None:
-        usuarios.remove(usuario)
-    
-    return {"status_borrado", "ok"}
